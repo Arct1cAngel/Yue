efa8624cb174ba754f493355b32c6dd53d413c65
Arctic <mhykolwave@gmail.com>
2024-05-07 22:06:16 -0300

    Updated bug with kill command where she would say she killed the user but not actually

--- Commands/YueOS/kill.js
+++ Commands/YueOS/kill.js
@@ -11,18 +11,11 @@ module.exports = {
         .setRequired(true)
     ),
     async execute(interaction) {
-        var DmAble = true;
-        var Target = interaction.options.getUser('target');
-        interaction.reply(`Okay, ill go out back and put <@${Target.id}> down like a dog.`);
-        Target.send(":boom: :gun: You're dead :)").catch(error => {
-        interaction.user.send("They've got barracades, I can't break through. I'm sorry to have failed you, boss. (This user has blocked me or has dms turned off)")
-        DmAble = false;
-        return;
-        }).catch(error => {
-          interaction.editReply(`There seems to be a lot of radio interference, I cannot kill that target. (Both you and your target have dms turned off)`)
-          DmAble = false;
-          return;
-        });
-      if (DmAble == true) interaction.user.send("The job is done, boss. You won't hear of them again.").catch(error => {interaction.editReply("Your dms seem to be off, boss. However I killed them anyway for you.")});
+      var dmAble = true;
+      var Target = interaction.options.getUser('target');
+      await interaction.reply(`Okay, ill go out back and put <@${Target.id}> down like a dog.`);
+      Target.send(`:boom: :gun: You're dead :)`).catch(error => { // Cannot DM Target
+        dmAble = false;
+      }).then(() => {if (dmAble == true) {interaction.followUp(`The job is done, boss. You won't hear of them again.`)} else {interaction.followUp(`They've got barracades, I can't break through. I'm sorry to have failed you, boss. (This user has blocked me or has dms turned off)`)}});
     }
 }
